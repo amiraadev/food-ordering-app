1e7741e2267a67be3c3e1c79e7ad03c8c02be5e6
amirticha <allagui_fawzi@outlook.fr>
2023-07-20 16:35:12 +0100

    traditionnal way

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -7,17 +7,17 @@ const data = [
   {
     id: 1,
     title: "always fresh & always crispy & always hot",
-    image: "/slide1.png",
+    image: "../../public/slide1.png",
   },
   {
     id: 2,
     title: "we deliver your order wherever you are in NY",
-    image: "/slide2.png",
+    image: "../../public/slide2.png",
   },
   {
     id: 3,
     title: "the best pizza to share with your family",
-    image: "/slide3.jpg",
+    image: "../../public/slide3.jpg",
   },
 ];
 
