2b721bba11779714fb47cd378e02ede0ec757276
amirticha <allagui_fawzi@outlook.fr>
2023-07-20 11:15:31 +0100

    md : login commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const LoginPage = () => {
   return (
-    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
+    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-7rem)] flex items-center justify-center">
       {/* BOX */}
       <div className=" h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
         {/* IMAGE CONTAINER */}
@@ -12,10 +12,10 @@ const LoginPage = () => {
           <Image src="/loginBg.png" alt="" fill className="object-cover"/>
         </div>
         {/* FORM CONTAINER */}
-        <div className="p-10 flex flex-col gap-8 md:w-1/2">
-          <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
+        <div className="p-10 flex flex-col gap-8 md:w-1/2 md:p-5">
+          <h1 className="font-bold text-xl xl:text-3xl text-center">Welcome</h1>
           <p>Log into your account or create a new one using social buttons</p>
-          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+          <button className="flex gap-2 p-4 ring-1 ring-orange-100 rounded-md">
             <Image
               src="/google.png"
               alt=""
@@ -25,7 +25,7 @@ const LoginPage = () => {
             />
             <span>Sign in with Google</span>
           </button>
-          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+          <button className="flex gap-2 p-4 ring-1 ring-blue-100 rounded-md">
             <Image
               src="/facebook.png"
               alt=""
