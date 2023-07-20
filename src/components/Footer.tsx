d9865c8948749ab54123519af9a90452d465d051
amirticha <allagui_fawzi@outlook.fr>
2023-07-20 11:27:15 +0100

    docker commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between '>
        <Link href="/" className='font-bold text-xl '>TOUNSINOU</Link>
-        <p  >2023 ALL RIGHTS RESERVED</p>
+        <p  >2023© ALL RIGHTS RESERVED</p>
     </div>
   )
 }
