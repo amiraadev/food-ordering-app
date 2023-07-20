db58f7b87ce5c3d1ecdd8a3af28304d814c04676
amirticha <allagui_fawzi@outlook.fr>
2023-07-20 13:49:19 +0100

    nextConfig:{output:'export'}

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-const nextConfig = {}
-
-module.exports = nextConfig
-
-
-*
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
