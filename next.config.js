4f47fe85e5937ec26227b846b074738d43940643
Amira Allagui <113999474+amirticha@users.noreply.github.com>
2023-08-11 18:21:18 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    basePath: '/food-ordering-app',
-    // output: 'export',
-
-    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-    // trailingSlash: true,
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+ basePath: '/food-ordering-app',
+}
+ 
+module.exports = nextConfig
 
   
